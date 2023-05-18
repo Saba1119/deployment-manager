0b7d3b1d90ffdd37a8aeb5f7e66653c455878192
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2023-05-18 18:39:29 +0530

    Add files via upload

---
+++ jinja/disk.py
@@ -0,0 +1,25 @@
+from jinja2 import Template, Environment, FileSystemLoader
+
+# Provide the actual values for the placeholders
+data = {
+    'name': 'saba',
+    'zone': 'us-central1-a',
+    'size': 10
+    'value': 'pd-standard',
+    'deletion_protection_enabled': 'true',
+    'physical_block_sixe_byte': '4096'
+}
+
+# Load the Jinja template file
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('persistent-disk.yaml')
+
+# Render the template with the provided data
+rendered_yaml = template.render(data)
+
+# Write the rendered YAML to a file
+with open('persistent-disk.yaml', 'w') as file:
+    file.write(rendered_yaml)
+
+# Print the rendered template
+print(rendered_yaml)
