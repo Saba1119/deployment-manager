ddc327d787317e3b0a737e81aad28a22d69ad079
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2023-05-18 18:20:23 +0530

    Add files via upload

---
+++ jinja/postgres.py
@@ -0,0 +1,34 @@
+from jinja2 import Template, Environment, FileSystemLoader
+
+# Provide the actual values for the placeholders
+data = {
+    'instance_name': 'sabanatarajan',
+    'region_name': ' us-central1',
+    'zone': 'us-central1-a',
+    'database_version': 'POSTGRES_14',
+    'instance_tier': 'db-f1-micro',
+    'backup_enabled': 'true',
+    'ipv4_enabled': 'true',
+    'storage_auto_resize': 'true',
+    'storage_size': 10,
+    'storage_type': 'SSD',
+    'environment': 'production',
+    'root_password': 'my-root-password',
+    'data_protection_enabled': 'true',
+    'maintenance_hour': 2,
+    'maintenance_day': 1
+}
+
+# Load the Jinja template file
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('postgres.yaml')
+
+# Render the template with the provided data
+rendered_yaml = template.render(data)
+
+# Write the rendered YAML to a file
+with open('postgres.yaml', 'w') as file:
+    file.write(rendered_yaml)
+
+# Print the rendered template
+print(rendered_yaml)
